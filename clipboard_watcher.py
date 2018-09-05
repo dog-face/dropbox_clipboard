fd1921bca66ed57282aab0e02976500bb08d7a80
mznco <admin@mznco.net>
2018-09-05 10:49:21 -0400

    fix CPU usage issues when run though automator

--- clipboard_watcher.py
+++ clipboard_watcher.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-
 import time
 import pyperclip
+from subprocess import call
 
 
 def is_dropbox_link(clipboard_content):
@@ -9,16 +8,14 @@ def is_dropbox_link(clipboard_content):
         clipboard_content is not None and
         clipboard_content.startswith("https://")
         and "www.dropbox.com" in clipboard_content
+        and "dl=0" in clipboard_content
     ):
         return True
     return False
 
 
 def make_dl_link(url):
-    pyperclip.copy(url.replace("www", "dl"))
-    print('converted link to dropbox dl link\n{}'.format(
-        url
-    ))
+    pyperclip.copy(url.replace("www", "dl").replace("?dl=0", ""))
 
 
 def watch_clipboard():
@@ -33,6 +30,7 @@ def watch_clipboard():
 
 
 def main():
+    call(['killall', 'ScriptMonitor'])  # see the readme
     watch_clipboard()
 
 
